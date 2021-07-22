32758841309d911c8521dfd6797026e8acc7c383
Nathan Leniz <nathan.leniz@mongodb.com>
2021-07-22 13:34:12 -0700

    c2

--- source/includes/usage-examples/findOneShort.go
+++ source/includes/usage-examples/findOneShort.go
@@ -1,5 +1,7 @@
 	// begin findOneUsageExample
 	coll := client.Database("sample_mflix").Collection("movies")
+	// result is an empty bson struct that will be populated with the
+	// matched document when the FindOne method returns
 	var result bson.D
 	err = coll.FindOne(ctx, bson.D{{"title", "The Room"}}).Decode(&result)
 	if err != nil {
